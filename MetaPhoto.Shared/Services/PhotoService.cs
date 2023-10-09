5c3c565e9a0154eb6a5c3597f76bf8418e01420c
Matthew David Elgert <mdelgert@yahoo.com>
2023-10-09 17:22:30 -0400

    Update comments

--- MetaPhoto.Shared/Services/PhotoService.cs
+++ MetaPhoto.Shared/Services/PhotoService.cs
@@ -1,51 +1,51 @@
-﻿namespace MetaPhoto.Shared.Services;
-
-public class PhotoService
+﻿namespace MetaPhoto.Shared.Services
 {
-    private readonly HttpClient _httpClient = new();
-
-    public async Task<Photo?> GetPhotoAsync(int photoId)
+    public class PhotoService
     {
-        var response = await _httpClient.GetAsync($"https://jsonplaceholder.typicode.com/photos/{photoId}");
-        response.EnsureSuccessStatusCode();
+        private readonly HttpClient _httpClient = new(); // Create an instance of HttpClient to make HTTP requests.
 
-        var json = await response.Content.ReadAsStringAsync();
-        var photo = JsonConvert.DeserializeObject<Photo>(json);
+        // Get a single photo by its ID asynchronously.
+        public async Task<Photo?> GetPhotoAsync(int photoId)
+        {
+            var response = await _httpClient.GetAsync($"https://jsonplaceholder.typicode.com/photos/{photoId}");
+            response.EnsureSuccessStatusCode(); // Ensure the HTTP request was successful.
 
-        return photo;
-    }
+            var json = await response.Content.ReadAsStringAsync(); // Read the JSON response.
+            var photo = JsonConvert.DeserializeObject<Photo>(json); // Deserialize the JSON to a Photo object.
 
-    public async Task<IEnumerable<Photo>?> GetPhotosAsync(int? limit = 25, int? offset = 0)
-    {
-        var response =
-            await _httpClient.GetAsync($"https://jsonplaceholder.typicode.com/photos?limit={limit}&offset={offset}");
-        response.EnsureSuccessStatusCode();
+            return photo; // Return the deserialized Photo object.
+        }
 
-        var json = await response.Content.ReadAsStringAsync();
-        var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(json);
+        // Get a list of photos asynchronously with optional limit and offset parameters.
+        public async Task<IEnumerable<Photo>?> GetPhotosAsync(int? limit = 25, int? offset = 0)
+        {
+            var response = await _httpClient.GetAsync($"https://jsonplaceholder.typicode.com/photos?limit={limit}&offset={offset}");
+            response.EnsureSuccessStatusCode(); // Ensure the HTTP request was successful.
 
-        return photos;
-    }
+            var json = await response.Content.ReadAsStringAsync(); // Read the JSON response.
+            var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(json); // Deserialize the JSON to a collection of Photo objects.
 
-    public async Task<IEnumerable<Photo>?> GetFilteredPhotosAsync(string? titleContains, string? albumTitleContains,
-        string? albumUserEmailEquals)
-    {
-        var filters = new List<string>();
+            return photos; // Return the deserialized collection of Photo objects.
+        }
 
-        if (!string.IsNullOrEmpty(titleContains)) filters.Add($"title={titleContains}");
+        // Get a list of photos asynchronously with optional filtering based on title, album title, and album user email.
+        public async Task<IEnumerable<Photo>?> GetFilteredPhotosAsync(string? titleContains, string? albumTitleContains, string? albumUserEmailEquals)
+        {
+            var filters = new List<string>();
 
-        if (!string.IsNullOrEmpty(albumTitleContains)) filters.Add($"album.title={albumTitleContains}");
+            if (!string.IsNullOrEmpty(titleContains)) filters.Add($"title={titleContains}");
+            if (!string.IsNullOrEmpty(albumTitleContains)) filters.Add($"album.title={albumTitleContains}");
+            if (!string.IsNullOrEmpty(albumUserEmailEquals)) filters.Add($"album.user.email={albumUserEmailEquals}");
 
-        if (!string.IsNullOrEmpty(albumUserEmailEquals)) filters.Add($"album.user.email={albumUserEmailEquals}");
+            var filterString = string.Join("&", filters);
 
-        var filterString = string.Join("&", filters);
+            var response = await _httpClient.GetAsync($"https://jsonplaceholder.typicode.com/photos?{filterString}");
+            response.EnsureSuccessStatusCode(); // Ensure the HTTP request was successful.
 
-        var response = await _httpClient.GetAsync($"https://jsonplaceholder.typicode.com/photos?{filterString}");
-        response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync(); // Read the JSON response.
+            var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(json); // Deserialize the JSON to a collection of Photo objects.
 
-        var json = await response.Content.ReadAsStringAsync();
-        var photos = JsonConvert.DeserializeObject<IEnumerable<Photo>>(json);
-
-        return photos;
+            return photos; // Return the deserialized collection of Photo objects based on applied filters.
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MetaPhoto
 MetaPhoto API Endpoint
+
+# Dependancies
+Microsoft.Azure.WebJobs.Extensions.OpenApi
+Microsoft.NET.Sdk.Functions
+Microsoft.NET.Test.Sdk
+xunit
+Newtonsoft.Json
